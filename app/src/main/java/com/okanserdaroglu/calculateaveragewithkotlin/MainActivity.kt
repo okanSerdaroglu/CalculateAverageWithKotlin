c09d1babeb92583e5efb5272b94586fb26d0232c
okanserdaroglu2@gmail.com <iserdaroglu@thy.com>
2019-11-07 16:06:30 +0300

    button click added

--- app/src/main/java/com/okanserdaroglu/calculateaveragewithkotlin/MainActivity.kt
+++ app/src/main/java/com/okanserdaroglu/calculateaveragewithkotlin/MainActivity.kt
@@ -2,11 +2,17 @@ package com.okanserdaroglu.calculateaveragewithkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun calculateAverage(view: View) {
+
+
     }
 }
