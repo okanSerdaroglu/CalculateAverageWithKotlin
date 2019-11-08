e9264d63eba93b6a22debe850d56713871db020d
okanserdaroglu2@gmail.com <iserdaroglu@thy.com>
2019-11-08 13:32:36 +0300

    new lesson layout added

--- app/src/main/java/com/okanserdaroglu/calculateaveragewithkotlin/MainActivity.kt
+++ app/src/main/java/com/okanserdaroglu/calculateaveragewithkotlin/MainActivity.kt
@@ -2,17 +2,34 @@ package com.okanserdaroglu.calculateaveragewithkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import java.util.zip.Inflater
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var inflater : LayoutInflater = LayoutInflater.from(this)
+
+        var newLessonView = inflater.inflate(R.layout.new_lesson_layout,null)
+
+
+
     }
 
     fun calculateAverage(view: View) {
 
 
     }
+
+    override fun onClick(view: View?) {
+
+
+
+    }
+
+
 }
