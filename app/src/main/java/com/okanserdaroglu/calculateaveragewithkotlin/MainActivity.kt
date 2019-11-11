57e5d4201d0bba9707a8a1fa108ab8871c6db97e
i_serdaroglu@thy.com <okanserdaroglu2@gmail.com>
2019-11-11 15:31:28 +0300

    average calculated

--- app/src/main/java/com/okanserdaroglu/calculateaveragewithkotlin/MainActivity.kt
+++ app/src/main/java/com/okanserdaroglu/calculateaveragewithkotlin/MainActivity.kt
@@ -7,13 +7,14 @@ import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.new_lesson_layout.*
 import kotlinx.android.synthetic.main.new_lesson_layout.view.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private val constantLessons =
         arrayOf("Math", "Turkish", "Physics", "Literature", "Algorithms", "History")
+
+    private var selectedLessonList = arrayListOf<Lesson>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +26,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         autoCompleteTextViewLessonName.setAdapter(adapter)
 
         buttonAddLesson.setOnClickListener(this)
-
+        buttonCalculateAverage.setOnClickListener(this)
 
 
     }
@@ -45,34 +46,58 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
             if (view.id == R.id.buttonAddLesson) {
 
-
-
                 var lessonName: String = autoCompleteTextViewLessonName.text.toString()
                 var credit = spinnerCredit.selectedItem.toString()
                 var charNote = spinnerNote.selectedItem.toString()
+
+                var selectedLesson: Lesson = Lesson(lessonName, credit.toInt(), charNote)
+
                 newLessonView.autoCompleteTextViewLessonNameNewLesson.setText(lessonName)
-                newLessonView.spinnerCreditNewLesson.setSelection(findSelectedIndexInSpinner(spinnerCredit,credit))
-                newLessonView.spinnerNoteNewLesson.setSelection(findSelectedIndexInSpinner(spinnerNote,charNote))
+                newLessonView.spinnerCreditNewLesson.setSelection(
+                    findSelectedIndexInSpinner(
+                        spinnerCredit,
+                        credit
+                    )
+                )
+                newLessonView.spinnerNoteNewLesson.setSelection(
+                    findSelectedIndexInSpinner(
+                        spinnerNote,
+                        charNote
+                    )
+                )
 
                 linearLayoutRootLayout.addView(newLessonView)
+
+                buttonCalculateAverage.visibility = View.VISIBLE
+
+                selectedLessonList.add(selectedLesson)
 
                 newLessonView.buttonRemoveLesson.setOnClickListener {
 
                     linearLayoutRootLayout.removeView(newLessonView)
+                    for (i in 0 until selectedLessonList.size) {
+                        if (newLessonView.autoCompleteTextViewLessonNameNewLesson.text.equals(selectedLessonList[i].lessonName)) {
+                            selectedLessonList.removeAt(i)
+                        }
+                    }
+
+                    if (linearLayoutRootLayout.childCount == 0) {
+                        buttonCalculateAverage.visibility = View.GONE
+                    }
 
                 }
 
 
-            } else if (view.id == R.id.buttonRemoveLesson) {
+            } else if (view.id == R.id.buttonCalculateAverage) {
 
-                linearLayoutRootLayout.removeView(newLessonView)
+                calculateAverageNote(selectedLessonList)
 
             }
         }
 
     }
 
-    fun convertCharToNote(charNote: String): Double { // converts char note to double value
+    private fun convertCharToNote(charNote: String): Double { // converts char note to double value
 
         return when (charNote) {
 
@@ -90,7 +115,30 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     }
 
-    fun findSelectedIndexInSpinner(spinner: Spinner, value: String): Int {
+    private fun calculateAverageNote(lessonList: ArrayList<Lesson>): Double {
+
+        var totalNote = 0.0
+        var totalCredit = 0.0
+        var averageNote = 0.0
+
+        if (lessonList.size == 0) {
+            return averageNote
+        }
+        for (i in 0 until lessonList.size) {
+
+            totalNote += lessonList[i].credit * convertCharToNote(lessonList[i].charNote)
+            totalCredit += lessonList[i].credit
+
+        }
+
+        averageNote = totalNote / totalCredit
+        textViewResult.text = averageNote.toString()
+
+        return averageNote
+
+    }
+
+    private fun findSelectedIndexInSpinner(spinner: Spinner, value: String): Int {
 
         var index = 0
 
